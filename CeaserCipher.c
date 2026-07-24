4eb3aa6b89c7320c01abd15905d3a568d8532701
DeepakAdhurthy <dadhurth@student.gitam.edu>
2026-07-24 11:47:45 +0530

    Add files via upload

---
+++ CeaserCipher.c
@@ -0,0 +1,39 @@
+﻿A.DEEPAK
+2024001778
+
+
+                                 Cryptography 
+
+
+Ceaser_cipher_encryption :
+
+
+#include<stdio.h>
+int main() {
+    char plaintext[100],ciphertext[100];
+    int key,i;
+    printf("Enter the plaintext: ");
+    scanf("%s",plaintext);
+    printf("Enter the key (integer): ");
+    scanf("%d",&key);
+    key=key%26;
+    for(i=0;plaintext[i]!='\0';i++) {
+        char ch=plaintext[i];
+       if(ch >= 'A' && ch <= 'Z')
+        {
+            ciphertext[i]=((ch-'A'+key) % 26) + 'A';
+        }
+        else if(ch >= 'a' && ch <= 'z')
+        {
+            ciphertext[i]=((ch-'a'+key) % 26) + 'a';
+        }
+        else
+        {
+            ciphertext[i]=ch;
+        }
+    }
+    ciphertext[i]='\0';
+    printf("\nPlain Text : %s\n",plaintext);
+    printf("Cipher Text: %s\n",ciphertext);
+    return 0;
+}
